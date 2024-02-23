a7649a81ffc1095253daa70e6193e54bfb510ae1
Luka Courmont <lcourmont@mdweb.fr>
2024-02-23 23:51:18 +0100

    ajout de babel dans le theme

--- wp-content/themes/emmagarnaud/functions.php
+++ wp-content/themes/emmagarnaud/functions.php
@@ -1,17 +1,31 @@
 <?php
+add_action('enqueue_block_editor_assets', function () {
+    wp_enqueue_script(
+        'custom-gutenberg-script',
+        get_template_directory_uri() . '/dist/js/main.js',
+        array( 'wp-blocks', 'wp-element', 'wp-components', 'wp-data', 'wp-compose', 'wp-editor' ),
+        filemtime(get_template_directory() . '/dist/js/main.js'),
+        true
+    );
+});
+
+
+function my_register_post_meta() {
+    register_post_meta( 'post', 'slick', array(
+        'show_in_rest' => true,
+        'single' => true,
+        'type' => 'number',
+    ) );
+}
+add_action( 'init', 'my_register_post_meta' );
 
 
 add_action('wp_enqueue_scripts', function () {
     wp_enqueue_style('mon_style', get_template_directory_uri() . '/style.css', false);
+    wp_enqueue_style('mon_style2', get_template_directory_uri() . '/src/css/style.css', false);
 });
 
 add_action( 'after_setup_theme', function() {
     register_nav_menu( 'main-menu', 'Menu principal' );
     add_theme_support( 'post-thumbnails' );
 });
-
-
-
-
-
-
---
+++ wp-content/themes/emmagarnaud/src/js/main.js
@@ -0,0 +1,54 @@
+console.log('script.js');
+
+(function(wp) {
+    let addFilter = wp.hooks.addFilter;
+    let createHigherOrderComponent = wp.compose.createHigherOrderComponent;
+    let Fragment = wp.element.Fragment;
+    let InspectorControls = wp.blockEditor.InspectorControls;
+    let PanelBody = wp.components.PanelBody;
+    let TextControl = wp.components.TextControl;
+    let withSelect = wp.data.withSelect;
+    let withDispatch = wp.data.withDispatch;
+
+    let withInspectorControl = createHigherOrderComponent((BlockEdit) => {
+        return withSelect((select, ownProps) => {
+            // Récupérez la valeur de la méta-donnée personnalisée
+            const { getEditedPostAttribute } = select('core/editor');
+            const meta = getEditedPostAttribute('meta');
+            return {
+                monChampPersonnalise: meta['slick'],
+            };
+        })(withDispatch((dispatch, ownProps, { select }) => {
+            // Fonction pour mettre à jour la méta-donnée
+            return {
+                setMonChampPersonnalise(value) {
+                    const { editPost } = dispatch('core/editor');
+                    const meta = select('core/editor').getEditedPostAttribute('meta');
+                    meta['slick'] = value;
+                    editPost({ meta });
+                }
+            };
+        })((props) => {
+            return (
+                <Fragment>
+                    <BlockEdit {...props} />
+                    <InspectorControls>
+                        <PanelBody title="Champ Personnalisé">
+                            <TextControl
+                                label="Entrez un nombre"
+                                value={props.monChampPersonnalise}
+                                onChange={(value) => props.setMonChampPersonnalise(value)}
+                            />
+                        </PanelBody>
+                    </InspectorControls>
+                </Fragment>
+            );
+        }));
+    }, 'withInspectorControl');
+
+    addFilter(
+        'editor.BlockEdit',
+        'mon-plugin/with-inspector-control',
+        withInspectorControl
+    );
+})(window.wp);
